5840465c81e0c025f7aee2476c28becc758cacf0
Obafemi <dadaobafemi26@gmail.com>
2025-07-03 20:40:20 +0100

    created get rotes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,8 +2,21 @@ import { createProduct, deleteProduct } from "../controllers/productController.j
 import VerifyToken from "../middleware/AuthMiddleware.js";
 import onlyAllow from '../middleware/RoleMiddleware.js'
 import express from "express";
+import ProductsSchema from "../models/ProductsSchema.js";
 
 const router = express.Router();
+
+router.get("/allProducts", async (req, res) => {
+    try {
+        const products = await ProductsSchema.find()
+
+        res.status(200).json({
+            data: products
+        })
+    } catch (error) {
+        res.status(400).json({message: "Error Fetching Products, check internet connection"})
+    }
+})
 
 router.post("/createProduct", VerifyToken, onlyAllow("Seller"), createProduct);
 
