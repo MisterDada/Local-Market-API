fddcb2c6784658e08a08d0a2a64b19b8d04bd178
Obafemi <dadaobafemi26@gmail.com>
2025-08-11 19:34:58 +0100

    added product category to user schema

--- models/ProductsSchema.js
+++ models/ProductsSchema.js
@@ -4,6 +4,7 @@ const ProductSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: String, required: true },
+  category: { type: String, required: true },
   seller: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
